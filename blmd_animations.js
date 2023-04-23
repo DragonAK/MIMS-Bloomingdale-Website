27fb8b51ff6db36a5f6316c1d3fb82fcbca21c29
Akshat K <89501286+DragonAK@users.noreply.github.com>
2023-04-23 15:48:25 +0530

    Update blmd_animations.js

--- blmd_animations.js
+++ blmd_animations.js
@@ -12,3 +12,22 @@ $(document).ready(function(){
         }
     });
 });
+
+const users = [
+    { username: 'akshat', password: 'secret1' },
+    { username: 'akki', password: 'secret2' }
+  ];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.querySelector('#login-button');
+    loginButton.addEventListener('click', () => {
+      const usernameInput = document.querySelector('#username-input').value;
+      const passwordInput = document.querySelector('#password-input').value;
+      const user = users.find(u => u.username === usernameInput && u.password === passwordInput);
+      if (user) {
+        window.location.href = 'https://docs.google.com/spreadsheets/d/1tsHMWAwJT5-y-mIsWhPbQpU7og5dYc6WD4NILZsR5TQ/edit?usp=sharing';
+      } else {
+        alert('Invalid username or password');
+      }
+    });
+  });
